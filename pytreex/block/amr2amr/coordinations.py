89eaeb654b13c22072142c2f9c74c2c3107f5f0b
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2015-06-30 12:20:01 +0200

    Notes for Silvie

--- pytreex/block/amr2amr/coordinations.py
+++ pytreex/block/amr2amr/coordinations.py
@@ -7,30 +7,34 @@
 #teprve napise API, ale formulaci uz prozradil
 from __future__ import unicode_literals
 
-from alex.components.nlg.tectotpl.core.block import Block
-from alex.components.nlg.tectotpl.core.exception import LoadingException
-from alex.components.nlg.tectotpl.tool.lexicon.cs import Lexicon
+# OD: Tohle si zkopiruj do templatu -- alex.components... je obsolete
+from pytreex.core.block import Block
+from pytreex.core.exception import LoadingException
 
-__author__ = "Silvie Cinkov·"
-__date__ = "2015"                                                                                                         
-                    
+__author__ = "Silvie Cinkov√°"
+__date__ = "2015"
+
+# OD: hlavni trida se VZDY musi jmenovat stejne jako jmeno souboru, ale muze byt 
+#     psana velkymi pismeny; jmeno souboru je VZDY jen malymi.
+#     Neprojde to, kdyz se soubor jmenuje "coordination" a trida "Coordination*s*"
+#     a podobne !
 class Coordinations(Block):
     """
     Build the AMR cooordination structure for two+ coordination
-    members according to the AMR manual, Conjunctions). 
-    Normally, a coordination has the conjunction on top and its members are 
+    members according to the AMR manual, Conjunctions).
+    Normally, a coordination has the conjunction on top and its members are
     numbered like this:
-    "boy and girl" 
+    "boy and girl"
    (a / and
       :op1 (b / boy)
       :op2 (g / girl)
       )
     "the boy shouted and left"
-    (a / and                                                                                                                                          
-    :op1 (c / shout-01)                                                                                                                              
-    :op2 (l / leave-01                                                                                                                               
+    (a / and
+    :op1 (c / shout-01)
+    :op2 (l / leave-01
            :ARG0 (b / boy)))
-  
+
     "the boy who shouted and left"
     (:b/boy
      ARG0-of(a / and
@@ -38,94 +42,108 @@ class Coordinations(Block):
      :op2 (l / leave-01)
      )
     )
-  
+
     On the other hand, coordinated non-clausal modifiers of a noun are
-    captured like this: 
+    captured like this:
     "big and heavy ball"
      (b / ball
    :mod (b2 / big)
    :mod (h / heavy))
-    """                                                                                      
-    
-    def __init__(self, scenario, args):                                               
-        "Constructor, just checking the argument values"                              
-        Block.__init__(self, scenario, args)                                          
-        if self.language is None:                                                     
-           raise LoadingException('Language must be defined!')                        
-        self.lexicon = Lexicon()                                                      
-                                                                                      
-    def process_amrnode(self, amrnode): #never change this function name              
-        """..."""                                                                            
-      tnode = amrnode.src_tnode                                         
-      if tnode is None: 
-        return
-      if tnode.nodetype != 'coap':
-        return
-      if tnode.functor == 'APPS':
-        return
-      tparent = tnode.parent     
-      amrchildren =  amrnode.get_children()   
-      tchildren = tnode.get_children()
-                
-      if CoordRules.is_noun_attribute(tnode, tparent=tparent): #tparent 
-      #vlevo je nazev parametru, jen nahodou uz mam stejne 
-      #pojmenovanou promennou, kterou jsem si predpocitala na zacatku 
-          amrparent = amrnode.parent
-          for amrchild in amrchildren:
-              amrchild.parent = amrparent #@property def(parent), node.py 
-              #l. 331, @parent setter, node.py l. 342 - tady vlastne nastavuju 
-              #hodnotu atributu 
-              amrchild.modifier = 'mod' #provides children with label
-          amrnode.remove() #removes the coordination amr node 
-      if not CoordRules.is_noun_attribute(tnode, tparent=tparent):
-          opnum = 1
-          for amrchild in amrchildren:
-              if (tchild is None and some_coordmember_there == False ): 
-              #pojmenovane entity, typ Franta Novak a Jarda Metelka  koordinace 
-              #dvou p/person a pod tim jmen, kterym teprve (tomu poslednimu) 
-              #odpovida koordinovany t-uzel   
-                  continue
-              if (tchild.is_member or (tchild is None and 
-                                        some_coordmember_there == True) 
-                                        and tparent.formeme != startswith('n:')):   
-                  amrchild.modifier = ":op%d"  % opnum
-                  opnum += 1    
-       
+    """
+
+    def __init__(self, scenario, args):
+        "Constructor, just checking the argument values"
+        Block.__init__(self, scenario, args)
+        if self.language is None:
+           raise LoadingException('Language must be defined!')
+       # OD: tady jsem Ti vymazal ten Lexicon, neni potreba, kdyz ho nepouzivas
+
+    def process_amrnode(self, amrnode): #never change this function name
+        """..."""
+        # OD: dodrzuj indentaci! kod metody je o *4* znaky napravo od "def"
+        tnode = amrnode.src_tnode
+        if tnode is None:
+            return
+        if tnode.nodetype != 'coap':
+            return
+        if tnode.functor == 'APPS':
+            return
+        tparent = tnode.parent
+        amrchildren =  amrnode.get_children()
+        tchildren = tnode.get_children()
+
+        if CoordRules.is_noun_attribute(tnode, tparent=tparent): #tparent
+            #vlevo je nazev parametru, jen nahodou uz mam stejne
+            #pojmenovanou promennou, kterou jsem si predpocitala na zacatku
+            amrparent = amrnode.parent
+            for amrchild in amrchildren:
+                amrchild.parent = amrparent #@property def(parent), node.py
+                #l. 331, @parent setter, node.py l. 342 - tady vlastne nastavuju
+                #hodnotu atributu
+                amrchild.modifier = 'mod' #provides children with label
+            amrnode.remove() #removes the coordination amr node
+
+        # OD: proc tu neni proste 'else' ?
+        if not CoordRules.is_noun_attribute(tnode, tparent=tparent):
+            opnum = 1
+            for amrchild in amrchildren:
+                if (tchild is None and some_coordmember_there == False ):
+                #pojmenovane entity, typ Franta Novak a Jarda Metelka  koordinace
+                #dvou p/person a pod tim jmen, kterym teprve (tomu poslednimu)
+                #odpovida koordinovany t-uzel
+                    continue
+                # OD: pozor na startswith, je to funkce stringu, ne globalni!
+                if (tchild.is_member or (tchild is None and
+                                         some_coordmember_there == True)
+                                         and tparent.formeme.startswith('n:')):
+                    # OD: dohodli jsme se, ze modifier je bez dvojtecky !
+                    amrchild.modifier = "op%d"  % opnum
+                    opnum += 1
+
 class CoordRules(object):
     """This class defines rules for distinguishing various types of coordination
     at the tnode level."""
 
     @staticmethod
     def is_noun_attribute(tnode, tparent=None, tchildren=None):
-      """Checks whether the given node is a noun attribute.
+        """Checks whether the given node is a noun attribute.
+  
+        Not "big and heavy ball", but yes "boy who shouted and left".
+        """
+        # OD: indentace je o *4* znaky! ne o 2, ne o cokoliv jinyho.
+        #     Python to mozna prezije, ale ja ne.
+        # OD: pozor na "=" a "==", viz nize !!!
+        if not tparent: # if tparent is None, condition evaluates to False.
+            tparent = tnode.parent()
+        # All children must be either adj: or n:
+        if not tchildren:
+            tchildren = tnode.get_children()
+        all_children_have_correct_formeme = True
+        for tchild in tchildren:
+            # OD: startswith je funkce stringu, neda se pouzivat, jaks to mela puvodne!!!
+            if not tchild.formeme.startswith('adj:') and not tchild.formeme.startswith('n:'):
+                all_children_have_correct_formeme = False
+                break
       
-      Not "big and heavy ball", but yes "boy who shouted and left". 
-      """
-      if not tparent: # if tparent = None, condition evaluates to False.  
-        tparent = tnode.parent()
-      # All children must be either adj: or n:
-      if not tchildren:
-        tchildren = tnode.get_children()
-      all_children_have_correct_formeme = True
-      for tchild in tchildren:
-        if tchild.formeme not in {startswith('adj:'), startswith('n:')}:
-          all_children_have_correct_formeme = False
-          break
-          
-    return (tnode.nodetype == 'coap' 
-              and tparent.formeme == startswith('n:')
-              and all_children_have_correct_formeme)       
-            
+        # OD: Tenhle return podle tebe nepatril k zadne metode -- dej pozor na tu indentaci!
+        # OD: opet, pozor na startswith
+        return (tnode.nodetype == 'coap'
+                  and tparent.formeme.startswith('n:')
+                  and all_children_have_correct_formeme)
+
     @staticmethod
     def is_coordinated_named_entity(amrnode):
-        if  (concept in ('person', 'location') and amrnode.src_tnode = None):
+        # OD: dej pozor na '=" a "==" -- prvni PRIRAZUJE, druhe testuje rovnost !
+        #     navic u None testuj pomoci "is".
+        # OD: kde jsi vzala promenou "concept" ??? dej pozor, ktere promenne mas...
+        if  (amrnode.concept in ('person', 'location') and amrnode.src_tnode is None):
             potential_tr_member_hosts = amrnode.get_descendants()
             some_coordmember_there = False
             for potential_tr_member_host in potential_tr_member_hosts:
                 candidate = potential_tr_member_host.src_tnode
                 if candidate.is_member == 1:
                     some_coordmember_there = True
-    return some_coordmember_there                  
-              
-              
-    
+        # OD: viz vyse -- return je soucasti metody, takze je indentovany oproti "def"!
+        return some_coordmember_there
+
+# OD: na konci souboru by nemely byt zbytecne prazdne radky
